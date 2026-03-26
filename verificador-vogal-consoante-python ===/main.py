8a0833317abc965bcde5ae5e0f7622f46ce7baaf
Henry F Ramos <137116833+hensla@users.noreply.github.com>
2026-03-26 19:22:14 -0300

    Add files via upload

---
+++ verificador-vogal-consoante-python ===/main.py
@@ -0,0 +1,5 @@
+L = input("coloque uma letra aleatoria MAIUSCULA: ")
+if L == "A" or L == "E" or L == "I" or L == "O" or L == "U":
+    print(f"{L} é uma vogal.")
+else:
+    print(f"{L} é uma consoante.")
